bd75d0e35907c6a454923b7db1b5601ec5ddc3da
bootta <bootta@yandex.ru>
2024-12-16 17:33:30 +0300

    fixed

--- src/Command/AddUserCommand.php
+++ src/Command/AddUserCommand.php
@@ -4,6 +4,9 @@ namespace Elenyum\Authorization\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Elenyum\Authorization\Entity\User;
+use Elenyum\Authorization\Entity\UserStatus;
+use Elenyum\Authorization\Repository\UserRepository;
+use Masterminds\HTML5\Exception;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -22,7 +25,8 @@ class AddUserCommand extends Command
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
-        private UserPasswordHasherInterface $passwordHasher
+        private UserPasswordHasherInterface $passwordHasher,
+        private UserRepository $repository
     ) {
         parent::__construct();
     }
@@ -31,26 +35,34 @@ class AddUserCommand extends Command
     {
         $this
             ->setDescription('Generate specification in OpenAPI, format: json')
-            ->addOption('login', 'l', InputOption::VALUE_OPTIONAL, 'email login', null)
-            ->addOption('password', 'p', InputOption::VALUE_OPTIONAL, 'password user', null);
+            ->addOption('login', 'l', InputOption::VALUE_REQUIRED, 'email login', null)
+            ->addOption('password', 'p', InputOption::VALUE_REQUIRED, 'password user', null)
+            ->addOption('status', 's', InputOption::VALUE_OPTIONAL, 'status user', UserStatus::Active->value)
+            ->addOption('roles', 'r', InputOption::VALUE_IS_ARRAY|InputOption::VALUE_OPTIONAL, 'roles user', []);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        $name = $input->getOption('name');
         $login = $input->getOption('login');
         $password = $input->getOption('password');
+        $status =  UserStatus::from($input->getOption('status'));
+        $roles = $input->getOption('roles');
 
-        if (!$name || !$login || !$password) {
+        if (!$login || !$password) {
             $io->error('Name, email, and password are required.');
             return Command::FAILURE;
         }
 
+        if (count($this->repository->findBy(['login' => $login])) > 0) {
+            $io->error('User with this login already exists');
+            return Command::FAILURE;
+        }
         $user = new User();
         $user->setLogin($login);
-        $user->setRoles(['adm']);
+        $user->setStatus($status);
+        $user->setRoles($roles);
 
         $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
         $user->setPassword($hashedPassword);
